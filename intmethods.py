82f2c694fcb4a20b1733e4516260c75b9160cde5
TheBjorn98 <bjrnarkaarevik@gmail.com>
2020-11-01 02:14:15 +0100

    added comments to intmethods and updated intMeth function return to return correctly formatted numpy arrays

--- intmethods.py
+++ intmethods.py
@@ -16,12 +16,32 @@ def stVerlet(p0, q0, dTdp, dVdq, dt):
 
 
 def intMeth(p0, q0, dTdp, dVdq, it_max, tol, func, step):
+    '''
+    Employs a specific integrator method to find the path of a particle
+    with the specified hamiltonian given by dTdp and dVdq
 
+    Inputs:
+    1. p0: initial momentum of the particle
+    2. q0: initial general position of the particle
+    3. dTdp: kinetic part of the hamiltonian diffed wrt. momentum
+    4. dVdq: potential part of the hamiltonian diffed wrt. position
+    5. it_max: number of steps the method will perform
+    6. tol: unused parameter, disregard
+    7. func: integrator method to be used, Euler or Strømer-Verlet
+    8. step: stepsize in time for the integrator method
+
+    Outputs:
+    1. ps: momentum as a function of time
+    2. qs: position as a function of time
+    '''
     ps = []
     qs = []
     ps.append(p0)
     qs.append(q0)
     p, q = p0, q0
+
+    lp, lq = np.shape(p0)[1], np.shape(q0)[1]
+
     diff = 100
     it = 0
     while it < it_max and diff > tol:
@@ -32,7 +52,8 @@ def intMeth(p0, q0, dTdp, dVdq, it_max, tol, func, step):
                 + np.linalg.norm(qs[-2] - qs[-1])) / 2
         it += 1
 
-    return np.array(ps), np.array(qs)
+    return (np.reshape(np.array(ps).T, (lp, it_max + 1)),
+            np.reshape(np.array(qs).T, (lq, it_max + 1)))
 
 
 def nonlinPend():
@@ -66,16 +87,16 @@ if __name__ == "__main__":
     q0 = np.array([1, 0])
 
     pen_dTdp, pen_dVdq = nonlinPend()
-    euler_pen_p, euler_pen_q = intMeth(p0[1], q0[1], pen_dTdp, pen_dVdq,
-                                       1000, 10**(-4), symEuler, 0.01)
-    verlet_pen_p, verlet_pen_q = intMeth(p0[1], q0[1], pen_dTdp, pen_dVdq,
-                                         1000, 10**(-4), stVerlet, 0.01)
+    euler_pen_p, euler_pen_q = intMeth(
+        p0[1], q0[1], pen_dTdp, pen_dVdq, 1000, 10**(-4), symEuler, 0.01)
+    verlet_pen_p, verlet_pen_q = intMeth(
+        p0[1], q0[1], pen_dTdp, pen_dVdq, 1000, 10**(-4), stVerlet, 0.01)
 
     kep_dTdp, kep_dVdq = keplerTwoBody()
-    euler_kep_p, euler_kep_q = intMeth(p0, q0, kep_dTdp, kep_dVdq,
-                                       10000, 10**(-4), symEuler, 0.01)
-    verlet_kep_p, verlet_kep_q = intMeth(p0, q0, kep_dTdp, kep_dVdq,
-                                         10000, 10**(-4), stVerlet, 0.01)
+    euler_kep_p, euler_kep_q = intMeth(
+        p0, q0, kep_dTdp, kep_dVdq, 10000, 10**(-4), symEuler, 0.01)
+    verlet_kep_p, verlet_kep_q = intMeth(
+        p0, q0, kep_dTdp, kep_dVdq, 10000, 10**(-4), stVerlet, 0.01)
 
     plt.plot(euler_pen_q, euler_pen_p)
     plt.xlabel("q")
